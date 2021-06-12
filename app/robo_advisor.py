6ca72cc4f6eb2732639a4cd66405cfd2829c1559
cys256 <84469159+cys256@users.noreply.github.com>
2021-06-12 16:13:05 -0400

    base case v1  - dates

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,8 +1,13 @@
-print("TODO: make a web request for some stock")
 
 import os
 from dotenv import load_dotenv
 import requests
+import json
+from datetime import datetime
+now = datetime.now()
+
+def to_usd(myprice):
+    return "${0:,.2f}".format(myprice)
 
 load_dotenv() # go get env vars from the .env file
 
@@ -10,23 +15,41 @@ load_dotenv() # go get env vars from the .env file
 ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
 
 # make a request
-symbol = "MSFT" # ask for a user input
+#symbol = "MSFT" 
+symbol = input("Please enter a valid stock ticker to see recommendation: ")
+t = symbol.upper()
 
 request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={ALPHAVANTAGE_API_KEY}"
 
 response = requests.get(request_url)
-print(type(response))
+#print(type(response))
+#print(response.status_code) #> 200
+#print(response.text)
 
-print(response.text)
+parsed_response = json.loads(response.text) # converts to dictionary
+
+last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+
+tsd = parsed_response["Time Series (Daily)"]
+
+dates = list(tsd.keys()) #TODO: sort to ensure latest day is first
+
+latest_day = dates[0]
+
+latest_close = tsd[latest_day]["4. close"]
+
+#breakpoint()
+
 
 print("-------------------------")
-print("SELECTED SYMBOL: XYZ")
+print("SELECTED SYMBOL: ", t)
 print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
-print("REQUEST AT: 2018-02-20 02:00pm")
+dt_string = now.strftime("%m/%d/%Y %H:%M %p")
+print("REQUEST AT:", dt_string)
 print("-------------------------")
-print("LATEST DAY: 2018-02-20")
-print("LATEST CLOSE: $100,000.00")
+print(f"LATEST DAY: {last_refreshed}")
+print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
 print("-------------------------")
